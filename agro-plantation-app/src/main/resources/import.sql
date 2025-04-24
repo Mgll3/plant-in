7904ba7b7b7c6ffa933c63909904339ea2ac258e
Mgll3 <miguel.alvarez1@udea.edu.co>
2025-04-23 19:20:16 -0500

    Montaje inicial del proyecto

---
+++ agro-plantation-app/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+-- Modifications To user_type
+
+INSERT INTO user_type (type) VALUES ('USER'), ('ADMIN'), ('PRODUCER'), ('PRODUCER_VIP');
+-- Poner el usuario que va a conectarse a la base de datos
+--REVOKE INSERT, UPDATE, DELETE ON user_type FROM 'username'@'hostname';
+
+-- Modifications state_request
+INSERT INTO state_request (state) VALUES ('PENDING'), ('ACCEPTED'), ('DECLINED');
+--REVOKE INSERT, UPDATE, DELETE ON state_request FROM 'username'@'hostname';
+
+INSERT INTO user (user_type_id, email, lastname, name, address, password) VALUES (2, 'admin@gmail.com', 'Admin', 'Admin', 'Direccion', 'admin');
